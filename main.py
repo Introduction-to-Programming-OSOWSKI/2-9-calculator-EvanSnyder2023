2d81ff6761288f7720a949cedd1e8a9b9e3c5125
EvanSnyder2023 <816547@mystma.org>
2021-01-14 16:35:30 +0000

    Calculate try 1

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-#WRITE YOUR CODE IN THIS FILE
+def calculate(x, y):
+    if "add":
+        return x + y
+    else "subtract":
+        return x - y
+    elif "multiply":
+        return x * y
+    elif "divide":
+        return x % y
